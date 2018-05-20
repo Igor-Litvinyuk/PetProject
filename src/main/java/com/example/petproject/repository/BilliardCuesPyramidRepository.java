03edc7c90c1044925ff8048c2703077320202e41
Igor <igorlitvinyuk1993@gmail.com>
2018-05-20 17:12:18 +0300

    Repository added

---
+++ src/main/java/com/example/petproject/repository/BilliardCuesPyramidRepository.java
@@ -0,0 +1,7 @@
+package com.example.petproject.repository;
+
+import com.example.petproject.model.BilliardCuesPyramid;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BilliardCuesPyramidRepository extends CrudRepository<BilliardCuesPyramid, Long> {
+}

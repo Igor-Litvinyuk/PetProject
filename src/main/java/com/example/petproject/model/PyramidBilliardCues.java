23853e203f0f21f3ffac247c01f81617af46055f
Igor <igorlitvinyuk1993@gmail.com>
2018-05-20 18:52:35 +0300

    model edited

--- src/main/java/com/example/petproject/model/PyramidBilliardCues.java
+++ src/main/java/com/example/petproject/model/PyramidBilliardCues.java
@@ -3,45 +3,64 @@ package com.example.petproject.model;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "billiard_cues_pyramid")
+@Table(name = "pyramid_billiard_cues")
 public class PyramidBilliardCues {
 
+    public PyramidBilliardCues() {
+    }
+
+    public PyramidBilliardCues(Integer id, String cue, String tree, String saw, String weight, String ferule,
+                               String sticker, String length, String incrustation, String producer, String price, String state) {
+        this.id = id;
+        this.cue = cue;
+        this.tree = tree;
+        this.saw = saw;
+        this.weight = weight;
+        this.ferule = ferule;
+        this.sticker = sticker;
+        this.length = length;
+        this.incrustation = incrustation;
+        this.producer = producer;
+        this.price = price;
+        this.state = state;
+    }
+
     @Id
-    @Column(name = "Номер")
+    @Column(name = "id")
     private Integer id;
 
-    @Column(name = "Кий")
+    @Column(name = "cue")
     private String cue;
 
-    @Column(name = "Дерево")
+    @Column(name = "tree")
     private String tree;
 
-    @Column(name = "Запил")
+    @Column(name = "saw")
     private String saw;
 
-    @Column(name = "Вес")
+    @Column(name = "weight")
     private String weight;
 
-    @Column(name = "Стакан")
-    private String glass;
+    @Column(name = "ferule")
+    private String ferule;
 
-    @Column(name = "Наклейка")
+    @Column(name = "sticker")
     private String sticker;
 
-    @Column(name = "Длина")
+    @Column(name = "length")
     private String length;
 
-    @Column(name = "Инкрустация")
+    @Column(name = "incrustation")
     private String incrustation;
 
-    @Column(name = "Производитель")
+    @Column(name = "producer")
     private String producer;
 
-    @Column(name = "Цена")
+    @Column(name = "price")
     private String price;
 
-    @Column(name = "Статус")
-    private String status;
+    @Column(name = "state")
+    private String state;
 
     public Integer getId() {
         return id;
@@ -83,12 +102,12 @@ public class PyramidBilliardCues {
         this.weight = weight;
     }
 
-    public String getGlass() {
-        return glass;
+    public String getFerule() {
+        return ferule;
     }
 
-    public void setGlass(String glass) {
-        this.glass = glass;
+    public void setFerule(String ferule) {
+        this.ferule = ferule;
     }
 
     public String getSticker() {
@@ -131,11 +150,11 @@ public class PyramidBilliardCues {
         this.price = price;
     }
 
-    public String getStatus() {
-        return status;
+    public String getState() {
+        return state;
     }
 
-    public void setStatus(String status) {
-        this.status = status;
+    public void setState(String state) {
+        this.state = state;
     }
 }

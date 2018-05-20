9de69c60d509be826aa1307f739cc90b545f2e30
Igor <igorlitvinyuk1993@gmail.com>
2018-05-20 18:52:08 +0300

    controller edited

--- src/main/java/com/example/petproject/controller/GreetingController.java
+++ src/main/java/com/example/petproject/controller/GreetingController.java
@@ -1,13 +1,20 @@
 package com.example.petproject.controller;
 
+import com.example.petproject.model.PyramidBilliardCues;
+import com.example.petproject.repository.PyramidBilliardCuesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+
+    @Autowired
+    private PyramidBilliardCuesRepository pyramidBilliardCuesRepository;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="user") String name, Map<String, Object> model) {
@@ -17,7 +24,21 @@ public class GreetingController {
 
     @GetMapping
     public String main(Map<String, Object> model){
-        model.put("some", "Web application for calculating concrete mixes");
+        Iterable<PyramidBilliardCues> pyramidBilliardCuesIterable = pyramidBilliardCuesRepository.findAll();
+        model.put("pyramidBilliardCues", pyramidBilliardCuesIterable);
+        return "main";
+    }
+
+    @PostMapping
+    public String addPyramidBilliardCueToDatabase(@RequestParam Integer id, @RequestParam String cue, @RequestParam String tree,
+                                                  @RequestParam String saw, @RequestParam String weight, @RequestParam String ferule,
+                                                  @RequestParam String sticker, @RequestParam String length, @RequestParam String incrustation,
+                                                  @RequestParam String producer, @RequestParam String price, @RequestParam String state,
+                                                  Map<String, Object> model){
+        PyramidBilliardCues pyramidBilliardCues = new PyramidBilliardCues(id, cue, tree, saw, weight, ferule, sticker, length, incrustation, producer, price, state);
+        pyramidBilliardCuesRepository.save(pyramidBilliardCues);
+        Iterable<PyramidBilliardCues> pyramidBilliardCuesIterable = pyramidBilliardCuesRepository.findAll();
+        model.put("pyramidBilliardCues", pyramidBilliardCuesIterable);
         return "main";
     }
 }

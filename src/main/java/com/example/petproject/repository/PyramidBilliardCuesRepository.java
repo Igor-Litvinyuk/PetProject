ce50611938cca4eb0999c8f3d400737cdb01b222
Igor <igorlitvinyuk1993@gmail.com>
2018-05-20 18:59:18 +0300

    Added method findByProducer -> PyramidBilliardCuesRepository

--- src/main/java/com/example/petproject/repository/PyramidBilliardCuesRepository.java
+++ src/main/java/com/example/petproject/repository/PyramidBilliardCuesRepository.java
@@ -3,5 +3,9 @@ package com.example.petproject.repository;
 import com.example.petproject.model.PyramidBilliardCues;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PyramidBilliardCuesRepository extends CrudRepository<PyramidBilliardCues, Long> {
+
+    List<PyramidBilliardCues> findByProducer(String producer);
 }

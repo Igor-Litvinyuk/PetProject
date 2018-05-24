766a8bec09815baa0317807873a68439fb85e9b2
Igor <igorlitvinyuk1993@gmail.com>
2018-05-24 12:43:53 +0300

    Field filename -> Message class

--- src/main/java/com/example/petproject/domain/Message.java
+++ src/main/java/com/example/petproject/domain/Message.java
@@ -15,6 +15,8 @@ public class Message {
     @JoinColumn(name = "user_id")
     private User author;
 
+    private String filename;
+
     public Message() {
     }
 
@@ -24,8 +26,16 @@ public class Message {
         this.tag = tag;
     }
 
-    public String getAuthorName(){
-        return author!= null ? author.getUsername() : "<none>";
+    public String getAuthorName() {
+        return author != null ? author.getUsername() : "<none>";
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
     }
 
     public void setText(String text) {
@@ -52,11 +62,11 @@ public class Message {
         this.tag = tag;
     }
 
-    public User getAuthor() {
-        return author;
+    public String getFilename() {
+        return filename;
     }
 
-    public void setAuthor(User author) {
-        this.author = author;
+    public void setFilename(String filename) {
+        this.filename = filename;
     }
 }
